29a9a0ecb2bc8e26e0fff2cefa21520f8ab45a1d
mahmud1628 <2105120@ugrad.cse.buet.ac.bd>
2025-04-22 18:00:37 +0600

    Completed print functions

--- Offline 1/2105120/2105120_SymbolTable.hpp
+++ Offline 1/2105120/2105120_SymbolTable.hpp
@@ -13,56 +13,72 @@ class SymbolTable {
         int num_buckets;
         int num_scopes;
     
-        public:
-            SymbolTable(int num_buckets) : num_buckets(num_buckets) {
-                num_scopes = 0;
-                currentScope = nullptr;
-                enterScope();
-            }
+    public:
+        SymbolTable(int num_buckets) : num_buckets(num_buckets) {
+            num_scopes = 0;
+            currentScope = nullptr;
+            enterScope();
+        }
 
-            ~SymbolTable() {
-                delete currentScope; // delete the current scope
-                // The destructor of ScopeTable will take care of deleting its parent scopes
-            }
+        ~SymbolTable() {
+            delete currentScope; // delete the current scope
+            // The destructor of ScopeTable will take care of deleting its parent scopes
+        }
 
-            void enterScope() {
-                ScopeTable * newScope = new ScopeTable(++num_scopes, num_buckets, currentScope);
-                currentScope = newScope;
-                cout << "New ScopeTable with id " << newScope->getId() << " created." << endl;
-            }
+        void enterScope() {
+            ScopeTable * newScope = new ScopeTable(++num_scopes, num_buckets, currentScope);
+            currentScope = newScope;
+            cout << "New ScopeTable with id " << newScope->getId() << " created." << endl;
+        }
 
-            void exitScope() {
-                if(currentScope->getParentScope() == nullptr) {
-                    return; // cannot exit the global scope
+        void exitScope() {
+            if(currentScope->getParentScope() == nullptr) {
+                return; // cannot exit the global scope
+            }
+            ScopeTable * parentScope = currentScope->getParentScope();
+            currentScope->setParentScope(nullptr); // avoid recursive deletion
+            delete currentScope; // delete the current scope
+            currentScope = parentScope; // move to the parent scope
+        }
+
+        bool insert(string name, string type) {
+            bool inserted = currentScope->insert(name, type);
+            return inserted;
+        }
+
+        bool remove(string name) {
+            bool removed = currentScope->deleteSymbol(name);
+            return removed;
+        }
+
+        SymbolInfo * lookup(string name) {
+            ScopeTable * scope = currentScope;
+            SymbolInfo * symbol;
+            while(scope != nullptr) {
+                symbol = scope->lookup(name);
+                if(symbol != nullptr) {
+                    return symbol;
                 }
-                ScopeTable * parentScope = currentScope->getParentScope();
-                currentScope->setParentScope(nullptr); // avoid recursive deletion
-                delete currentScope; // delete the current scope
-                currentScope = parentScope; // move to the parent scope
+                scope = scope->getParentScope();
             }
+            return nullptr; // not found
+        }
 
-            bool insert(string name, string type) {
-                bool inserted = currentScope->insert(name, type);
-                return inserted;
-            }
+        void printCurrentScope(bool tabs = false) {
+            int numberOfTabs;
+            tabs ? numberOfTabs = 1 : numberOfTabs = 0;
+            currentScope->print(numberOfTabs);
+        }
 
-            bool remove(string name) {
-                bool removed = currentScope->deleteSymbol(name);
-                return removed;
+        void printAllScopes(bool tabs = false) {
+            ScopeTable * scope = currentScope;
+            int numberOfTabs = 0;
+            while(scope != nullptr) {
+                tabs ? numberOfTabs++ : 0;
+                scope->print(numberOfTabs);
+                scope = scope->getParentScope();
             }
-
-            SymbolInfo * lookup(string name) {
-                ScopeTable * scope = currentScope;
-                SymbolInfo * symbol;
-                while(scope != nullptr) {
-                    symbol = scope->lookup(name);
-                    if(symbol != nullptr) {
-                        return symbol;
-                    }
-                    scope = scope->getParentScope();
-                }
-                return nullptr; // not found
-            }
+        }
 
 };
 

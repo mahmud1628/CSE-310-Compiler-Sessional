9c19f4f55855801d21c5a4a64b355aa30c32f2e7
mahmud1628 <2105120@ugrad.cse.buet.ac.bd>
2025-04-22 15:48:09 +0600

    Added the updated hash function

--- Offline 1/2105120/2105120_hash.hpp
+++ Offline 1/2105120/2105120_hash.hpp
@@ -18,6 +18,16 @@ class Hash {
         
             return hash;
         }
+
+        static unsigned int SDBMHash ( string str , unsigned int num_buckets ) {
+            unsigned int hash = 0;
+            unsigned int len = str . length () ;
+            for ( unsigned int i = 0; i < len ; i ++) {
+                hash = (( str [ i ]) + ( hash << 6) + ( hash << 16) - hash ) %
+                num_buckets ;
+            }
+            return hash ;
+        }
 };
 
 

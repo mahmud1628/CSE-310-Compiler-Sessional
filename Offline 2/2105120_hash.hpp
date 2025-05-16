6e318b7bbf478db879c534ba3af9a5c4e5571e0d
mahmud1628 <2105120@ugrad.cse.buet.ac.bd>
2025-05-16 11:08:40 +0600

    New hash function needed for offline 2

--- Offline 2/2105120_hash.hpp
+++ Offline 2/2105120_hash.hpp
@@ -47,6 +47,16 @@ public:
 
         return hash;
     }
+
+    static unsigned int sdbmHash(const char *p) {
+        unsigned int hash = 0;
+        auto *str = (unsigned char *) p;
+        int c{};
+        while ((c = *str++)) {
+        hash = c + (hash << 6) + (hash << 16) - hash;
+        }
+        return hash;
+    }
 };
 
 #endif // _HASH_HPP_
